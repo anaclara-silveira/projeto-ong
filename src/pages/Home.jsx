68c0c7d54ec690123a34726c402fcc8ad0bf1947
anaclara-silveira <anasclarauni@gmail.com>
2025-10-30 21:17:01 -0300

    Removendo Lorem Ipsum

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,9 @@ function Home() {
           <h2 style={{ letterSpacing: '15px' }}>Bem vindo a</h2>
           <h2 style={{ fontSize: '80px' }}>ONG Doce Lar</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam et minus ad possimus amet magni, iure,
-            aliquam minima, odit ea voluptatum neque dignissimos nobis quaerat blanditiis cupiditate quae optio
-            perferendis.
+            Somos uma organização dedicada ao cuidado e proteção de animais em situação de vulnerabilidade. 
+            Nossa missão é resgatar, reabilitar e encontrar lares responsáveis para nossos amigos de quatro patas, 
+            promovendo o bem-estar animal e fortalecendo o vínculo entre humanos e animais.
           </p>
           <div className="drop">
             <Link to="/cadastro">Seja um Voluntário</Link>
@@ -29,9 +29,9 @@ function Home() {
             <img src="/img/missao.png" alt="icone de missao" />
             <h3>MISSÃO</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque blanditiis inventore voluptates
-              dolorum facilis dolore repellendus. Ullam animi, neque nesciunt maxime mollitia vitae. Quas repellat
-              consectetur qui praesentium cumque molestias!
+              Resgatar, proteger e promover o bem-estar de animais abandonados, oferecendo cuidados veterinários, 
+              abrigo temporário e buscando lares responsáveis através de processos rigorosos de adoção. 
+              Trabalhamos incansavelmente para reduzir o abandono e o maus-tratos animal em nossa comunidade.
             </p>
           </div>
 
@@ -39,9 +39,9 @@ function Home() {
             <img src="/img/valores.png" alt="icone de valores" />
             <h3>VALORES</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam distinctio, minima, deserunt nobis
-              facilis hic quae reprehenderit cupiditate tempore nisi velit commodi sunt. Quis ipsa ex doloremque
-              ullam ducimus in!
+              Compaixão, responsabilidade, transparência e dedicação são os pilares que guiam nosso trabalho. 
+              Acreditamos no poder transformador do cuidado e do respeito aos animais, promovendo educação 
+              sobre guarda responsável e fortalecendo a consciência sobre o bem-estar animal na sociedade.
             </p>
           </div>
 
@@ -49,9 +49,9 @@ function Home() {
             <img src="/img/vision.png" alt="icone de visão" />
             <h3>VISÃO</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem eum cupiditate natus veniam nobis,
-              assumenda quasi necessitatibus dolorem similique. Tenetur et quisquam suscipit tempore quidem sint
-              quibusdam enim esse praesentium!
+              Ser referência em proteção animal, criando uma comunidade onde não existam mais animais abandonados 
+              nas ruas. Almejamos um futuro onde todos os animais tenham lares amorosos e responsáveis, 
+              e onde a população esteja consciente sobre guarda responsável e bem-estar animal.
             </p>
           </div>
         </div>
